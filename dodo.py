1b11df4517ac671cb1d5a455580fed6ee7d05742
Ernest Izdebski <izdebski@andrews.edu>
2026-03-27 18:38:05 -0400

    Add `doit` build system
    I eventually just didn't want to learn something new.
    
    Closes #3
    Closes #6

---
+++ dodo.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+def Action(*args: str | Path) -> list[str]:
+    result: list[str] = []
+    for value in args:
+        if isinstance(value, Path):
+            value = value.as_posix()
+        result.append(value)
+    return result
+
+def task_compile():
+    working_directory = Path("src")
+    targets_directory = Path("target")
+    variant_directory = working_directory / "target"
+    variants = {path for path in variant_directory.glob("*.typ") if not path.name.startswith("_")}
+    sources = list({*working_directory.rglob("*.typ")} - variants)
+    for variant in variants:
+        name = variant.name.removesuffix(variant.suffix)
+        target = targets_directory / (name + ".pdf")
+        yield {
+            "name": name,
+            "actions": [
+                Action(
+                    "tinymist",
+                    "compile",
+                    "--save-lock",
+                    "--root",
+                    "src/",
+                    "--",
+                    variant,
+                    target
+                )
+            ],
+            "file_dep": [variant] + sources,
+            "targets": (target, )
+        }
